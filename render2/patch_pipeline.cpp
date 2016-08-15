2a1373bfdfd22067725126583419e641cf57a519
Phantomical <phantom@lynches.ca>
2016-08-14 22:48:10 -0400

    Reworked patch_pipeline internals

--- render2/patch_pipeline.cpp
+++ render2/patch_pipeline.cpp
@@ -7,168 +7,7 @@ namespace planet_engine
 {
 	static size_t n = 0;
 
-	template<typename T>
-	bool is_failed(const T& v)
-	{
-		return v.state == T::ERROR_STATE;
-	}
-
-#define any_of_switch(exec, func) do { \
-	switch (exec.active_index()) { \
-		case 0: func(0); break; \
-		case 1: func(1); break; \
-		case 2: func(2); break; \
-		case 3: func(3); break; \
-	}} while(0)
-
-	void patch_pipeline::_upsample_state::step()
-	{
-		static constexpr size_t NUM_INVOCATIONS = (NUM_VERTICES + SHADER_GROUP_SIZE - 1) / SHADER_GROUP_SIZE;
-
-		switch (state)
-		{
-		case START:
-		{
-			// Get offset for new patch
-			offset = pipeline->_manager.alloc_block();
-
-			glUseProgram(pipeline->_upsampler);
-			// Uniform values
-			glm::dvec3 positions[] = {
-				children[0]->pos,
-				children[1]->pos,
-				children[2]->pos,
-				children[3]->pos
-			};
-			GLuint _offsets[4];
-
-			for (size_t i = 0; i < 4; ++i)
-			{
-				auto it = pipeline->_offsets.find(children[i]);
-				if (it == pipeline->_offsets.end())
-				{
-					assert(false);
-					state = ERROR_STATE;
-					return;
-				}
-
-				_offsets[i] = it->second;
-			}
-
-			// Bind and fill uniform buffer
-			GLuint uniforms;
-			glGenBuffers(1, &uniforms);
-			glBindBuffer(GL_UNIFORM_BUFFER, uniforms);
-			glBufferData(GL_UNIFORM_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
-
-			// Bind output
-			glBindBufferRange(GL_SHADER_STORAGE_BUFFER, 0, pipeline->_manager.buffer(),
-				offset * pipeline->_manager.block_size(), pipeline->_manager.block_size());
-
-			// Bind inputs
-			for (GLuint i = 0; i < 4; ++i)
-			{
-				glBindBufferRange(GL_SHADER_STORAGE_BUFFER, i + 1, pipeline->_manager.buffer(),
-					_offsets[i] * pipeline->_manager.block_size(), pipeline->_manager.block_size());
-			}
-
-			// Dispatch upsample shader 
-			glDispatchCompute(NUM_INVOCATIONS, 1, 1);
-			// Make effects visible to other shaders down the pipeline
-			glMemoryBarrier(GL_VERTEX_ATTRIB_ARRAY_BARRIER_BIT | GL_SHADER_STORAGE_BARRIER_BIT);
-
-			glDeleteBuffers(1, &uniforms);
-
-			state = DONE;
-
-			OutputDebug("[PIPELINE] Upsampling patch ", target_patch.get(), '\n');
-
-			OutputDebug("[PIPELINE] Removing patch ", children[0].get(), '\n');
-			OutputDebug("[PIPELINE] Removing patch ", children[1].get(), '\n');
-			OutputDebug("[PIPELINE] Removing patch ", children[2].get(), '\n');
-			OutputDebug("[PIPELINE] Removing patch ", children[3].get(), '\n');
-			break;
-		}
-		case DONE:
-			break;
-		case ERROR_STATE:
-			break;
-		default:
-			break;
-		}
-	}
-	void patch_pipeline::_remove_state::step()
-	{
-		switch (state)
-		{
-		case START:
-		{
-			auto it = pipeline->_offsets.find(target_patch);
-			if (it == pipeline->_offsets.end())
-			{
-				if (ctr++ < 16)
-					state = START;
-				else
-				{
-					state = ERROR_STATE;
-					OutputDebug("[PIPELINE][ERROR] Unable to find offset for patch ", target_patch.get(), ".\n");
-				}
-				return;
-			}
-
-			offset = it->second;
-			pipeline->_offsets.erase(it);
-
-			state = DONE;
-
-			OutputDebug("[PIPELINE] Removed patch ", target_patch.get(), ". There are ", --n, " patches.\n");
-			break;
-		}
-		case DONE:
-			break;
-		case ERROR_STATE:
-			break;
-		default:
-			break;
-		}
-	}
-	
-	patch_pipeline::_upsample_state::_upsample_state(std::shared_ptr<patch> p, patch_pipeline* pipeline) :
-		state(START),
-		pipeline(pipeline),
-		target_patch(p)
-	{
-		if (!p->subdivided())
-		{
-			state = ERROR_STATE;
-		}
-		else
-		{
-			if (p->nw->subdivided() ||
-				p->ne->subdivided() ||
-				p->sw->subdivided() ||
-				p->se->subdivided())
-			{
-				state = ERROR_STATE;
-			}
-			else
-			{
-				children[0] = p->nw;
-				children[1] = p->ne;
-				children[2] = p->sw;
-				children[3] = p->se;
-			}
-		}
-	}
-	patch_pipeline::_remove_state::_remove_state(std::shared_ptr<patch> p, patch_pipeline* pipeline) :
-		state(START),
-		pipeline(pipeline),
-		target_patch(p),
-		ctr(0)
-	{
-
-	}
-
+	/* Patch Pipeline */
 	void patch_pipeline::gen_vertices(GLuint buffers[2], std::shared_ptr<patch> patch, GLuint* offset)
 	{
 		glGenBuffers(2, buffers);
@@ -216,277 +55,97 @@ namespace planet_engine
 
 		// Delete buffers
 		glDeleteBuffers(2, buffers);
+
+		buffers[0] = 0;
+		buffers[1] = 0;
 	}
 
-	void patch_pipeline::subdivide_state::gen_vertices(size_t idx)
-	{
-		pipeline->gen_vertices(buffers[idx], children[idx], &offsets[idx]);
-
-		++counter;
-	}
-	void patch_pipeline::subdivide_state::gen_mesh(size_t idx)
-	{
-		pipeline->gen_mesh(buffers[idx], children[idx], &offsets[idx]);
-
-		++counter;
-	}
-
-	void patch_pipeline::merge_state::gen_vertices()
-	{
-		pipeline->gen_vertices(buffers, parent, &offset);
-
-		++counter;
-	}
-	void patch_pipeline::merge_state::gen_mesh()
-	{
-		glUseProgram(pipeline->_meshgen);
-		// Bind input buffer
-		glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 0, vertex_buffer);
-		// Bind output buffer range
-		glBindBufferRange(GL_SHADER_STORAGE_BUFFER, 1, pipeline->manager().buffer(),
-			offset * pipeline->manager().block_size(), pipeline->manager().block_size());
-		// Bind uniforms
-		glBindBufferBase(GL_UNIFORM_BUFFER, 0, uniforms);
-		// Dispatch compute shader to fill the output buffer range
-		glDispatchCompute(NUM_INVOCATIONS, 1, 1);
-		glMemoryBarrier(GL_VERTEX_ATTRIB_ARRAY_BARRIER_BIT | GL_SHADER_STORAGE_BARRIER_BIT);
-
-		// Delete buffers
-		glDeleteBuffers(1, &uniforms);
-		glDeleteBuffers(1, &vertex_buffer);
-
-		++counter;
-	}
-
-	void patch_pipeline::generate_state::gen_vertices()
-	{
-		pipeline->gen_vertices(buffers, patch, &offset);
-		
-		++counter;
-	}
-	void patch_pipeline::generate_state::gen_mesh()
-	{
-		pipeline->gen_mesh(buffers, patch, &offset);
-
-		++counter;
-	}
-
-	bool patch_pipeline::merge_state::can_finalize()
-	{
-		return counter == 2;
-	}
-	void patch_pipeline::merge_state::get_state(update_state& ustate)
-	{
-		for (size_t i = 0; i < 4; ++i)
-		{
-			MoveCommand mvcmd;
-			mvcmd.is_new = GL_FALSE;
-			mvcmd.source = 0;
-			mvcmd.dest = offsets[i];
-
-			ustate.movecommands.push_back(mvcmd);
-
-			auto it = pipeline->_offsets.find(children[i]);
-			if (it == pipeline->_offsets.end())
-				assert(false);
-			pipeline->_offsets.erase(it);
-
-			pipeline->manager().dealloc_block(offsets[i]);
-		}
-
-		{
-			MoveCommand mvcmd;
-			mvcmd.is_new = GL_TRUE;
-			mvcmd.source = ustate.commands.size();
-			mvcmd.dest = offset;
-
-			DrawElementsIndirectCommand cmd;
-			cmd.count = NUM_ELEMENTS;
-			cmd.instanceCount = 1;
-			cmd.firstIndex = 0;
-			cmd.baseVertex = pipeline->manager().block_size() / VERTEX_SIZE * offset;
-			cmd.baseInstance = 0;
-
-			ustate.movecommands.push_back(mvcmd);
-			ustate.commands.push_back(cmd);
-
-			pipeline->_offsets.insert(std::make_pair(parent, offset));
-		}
-	}
-
-	bool patch_pipeline::subdivide_state::can_finalize()
-	{
-		return counter == 8;
-	}
-	void patch_pipeline::subdivide_state::get_state(update_state& ustate)
-	{
-		for (size_t i = 0; i < 4; ++i)
-		{
-			MoveCommand mvcmd;
-			mvcmd.is_new = GL_TRUE;
-			mvcmd.source = ustate.commands.size();
-			mvcmd.dest = offsets[i];
-
-			DrawElementsIndirectCommand cmd;
-			cmd.count = NUM_ELEMENTS;
-			cmd.instanceCount = 1;
-			cmd.firstIndex = 0;
-			cmd.baseVertex = pipeline->manager().block_size() / VERTEX_SIZE * offsets[i];
-			cmd.baseInstance = 0;
-
-			ustate.movecommands.push_back(mvcmd);
-			ustate.commands.push_back(cmd);
-
-			pipeline->_offsets.insert(std::make_pair(children[i], offsets[i]));
-
-		}
-
-		{
-			pipeline->manager().dealloc_block(offset);
-
-			MoveCommand mvcmd;
-			mvcmd.is_new = GL_FALSE;
-			mvcmd.source = 0;
-			mvcmd.dest = offset;
-
-			auto it = pipeline->_offsets.find(parent);
-			if (it == pipeline->_offsets.end())
-				assert(false);
-			pipeline->_offsets.erase(it);
-
-			ustate.movecommands.push_back(mvcmd);
-		}
-	}
-
-	bool patch_pipeline::remove_state::can_finalize()
-	{
-		return true;
-	}
-	void patch_pipeline::remove_state::get_state(update_state& ustate)
-	{
-		auto it = pipeline->_offsets.find(patch);
-		if (it == pipeline->_offsets.end())
-			assert(false);
-		GLuint offset = it->second;
-		pipeline->_offsets.erasae(it);
-
-		pipeline->manager().dealloc_block(offset);
-
-		MoveCommand mvcmd;
-		mvcmd.is_new = GL_FALSE;
-		mvcmd.source = 0;
-		mvcmd.dest = offset;
-
-		ustate.movecommands.push_back(mvcmd);
-	}
-
-	bool patch_pipeline::generate_state::can_finalize()
-	{
-		return counter == 2;
-	}
-	void patch_pipeline::generate_state::get_state(update_state& ustate)
-	{
-		MoveCommand mvcmd;
-		mvcmd.is_new = GL_TRUE;
-		mvcmd.source = ustate.commands.size();
-		mvcmd.dest = offset;
-
-		DrawElementsIndirectCommand cmd;
-		cmd.count = NUM_ELEMENTS;
-		cmd.instanceCount = 1;
-		cmd.firstIndex = 0;
-		cmd.baseVertex = pipeline->manager().block_size() / VERTEX_SIZE * offset;
-		cmd.baseInstance = 0;
-
-		ustate.movecommands.push_back(mvcmd);
-		ustate.commands.push_back(cmd);
-
-		pipline->_offsets.insert(std::make_pair(patch, offset));
-	}
-
-	void patch_pipeline::subdivide(std::shared_ptr<patch> patch)
-	{
-		std::shared_ptr<subdivide_state> ptr = std::make_shared<subdivide_state>(patch, this);
-
-		for (size_t i = 0; i < 4; ++i)
-		{
-			_job_queue.push([=]() {
-				ptr->gen_vertices(i);
-			});
-			_job_queue.push([=]() {
-				ptr->gen_mesh(i);
-			});
-		}
-
-		_exec_queue.emplace(ptr);
-	}
-	void patch_pipeline::merge(std::shared_ptr<patch> patch)
-	{
-		std::shared_ptr<merge_state> ptr = std::make_shared<merge_state>(patch, this);
-
-		_job_queue.push([=]() {
-			ptr->gen_vertices();
-		});
-		_job_queue.push([=]() {
-			ptr->gen_mesh();
-		});
-
-		_exec_queue.push(ptr);
-	}
 	void patch_pipeline::generate(std::shared_ptr<patch> patch)
 	{
-		std::shared_ptr<generate_state> ptr = std::make_shared<generate_state>(patch, this);
+		auto ptr = std::make_shared<generate_state>(patch, this);
 
-		_job_queue.push([=]() {
-			ptr->gen_vertices();
-		});
-		_job_queue.push([=]() {
-			ptr->gen_mesh();
+		_job_queue.push([ptr = ptr, pipeline = this]() {
+			if (ptr->counter == CANCELLED_COUNTER_VALUE)
+				return false;
+
+			pipeline->gen_vertices(ptr->buffers, ptr->target, &ptr->offset);
+			++ptr->counter;
+
+			return true;
 		});
 
-		_exec_queue.push(exec_type(ptr));
+		_job_queue.push([ptr = ptr, pipeline = this]() {
+			if (ptr->counter == CANCELLED_COUNTER_VALUE)
+				return false;
+
+			pipeline->gen_mesh(ptr->buffers, ptr->target, &ptr->offset);
+
+			return true;
+		});
+
+		_exec_queue.push_back(ptr);
 	}
 	void patch_pipeline::remove(std::shared_ptr<patch> patch)
 	{
-		std::shared_ptr<remove_state> ptr = std::make_shared<remove_state>(patch, this);
+		auto ptr = std::make_shared<remove_state>(patch, this);
 
-		_exec_queue.push(exec_type(ptr));
+		_exec_queue.push_back(ptr);
 	}
 
 	update_state patch_pipeline::process(size_t n)
 	{
-#define finalize(idx) \
-	finalizable = exec.get<exec_type::type_at<idx>>()->can_finalize(); 
-#define get_state(idx) exec.get<exec_type::type_at<idx>>()->get_state(ustate)
-
-		for (size_t i = 0; i < n && !_job_queue.empty(); ++i)
+		for (size_t i = 0; i < n && !_job_queue.empty();)
 		{
-			_job_queue.front()();
+			bool val = _job_queue.front()();
 			_job_queue.pop();
+
+			if (!val)
+				++i;
 		}
 
 		update_state ustate;
 
-		bool finalizable = true;
-
-		while (finalizable && !_exec_queue.empty())
+		bool cond = true;
+		while (!_exec_queue.empty() && cond)
 		{
 			auto& exec = _exec_queue.front();
 
-			any_of_switch(exec, finalize);
-
-			if (finalizable)
+			switch (exec.active_index())
 			{
-				any_of_switch(exec, get_state);
-
-				_exec_queue.pop();
+			case 0:
+			{
+				auto& val = *exec.get<exec_type::type_at<0>>();
+				if (val.counter == CANCELLED_COUNTER_VALUE)
+					_exec_queue.pop_front();
+				else if (val.can_finalize())
+				{
+					val.finalize(ustate);
+					_exec_queue.pop_front();
+				}
+				else
+					cond = false;
+				break;
+			}
+			case 1:
+			{
+				auto& val = *exec.get<exec_type::type_at<1>>();
+				if (val.counter == CANCELLED_COUNTER_VALUE)
+					_exec_queue.pop_front();
+				else if (val.can_finalize())
+				{
+					val.finalize(ustate);
+					_exec_queue.pop_front();
+				}
+				else
+					cond = false;
+				break;
+			}
+			default:
+				assert(false);
 			}
 		}
 
 		return ustate;
-
-#undef finalize
-#undef get_state
 	}
 
 	buffer_manager& patch_pipeline::manager()
@@ -534,16 +193,80 @@ namespace planet_engine
 		glDeleteProgram(_vertex_gen);
 		glDeleteProgram(_upsampler);
 	}
-
-	bool patch_pipeline::can_upsample(std::shared_ptr<patch> p)
+	
+	void patch_pipeline::generate_state::cancel()
 	{
-		if (!p->subdivided())
-			return false;
-		if (p->nw->subdivided() ||
-			p->ne->subdivided() ||
-			p->sw->subdivided() ||
-			p->se->subdivided())
-			return false;
+		counter = CANCELLED_COUNTER_VALUE;
+
+		if (buffers[0] != 0)
+			glDeleteBuffers(2, buffers);
+	}
+	void patch_pipeline::remove_state::cancel()
+	{
+		counter = CANCELLED_COUNTER_VALUE;
+	}
+
+	bool patch_pipeline::generate_state::can_finalize() const
+	{
+		return counter == 2;
+	}
+	bool patch_pipeline::remove_state::can_finalize() const
+	{
 		return true;
+	}
+
+	void patch_pipeline::generate_state::finalize(update_state& ustate)
+	{
+		MoveCommand mvcmd;
+		mvcmd.is_new = GL_TRUE;
+		mvcmd.source = ustate.commands.size();
+		mvcmd.dest = offset;
+
+		DrawElementsIndirectCommand cmd;
+		cmd.count = NUM_ELEMENTS;
+		cmd.instanceCount = 1;
+		cmd.firstIndex = 0;
+		cmd.baseVertex = pipeline->manager().block_size() / VERTEX_SIZE * offset;
+		cmd.baseInstance = 0;
+
+		ustate.movecommands.push_back(mvcmd);
+		ustate.commands.push_back(cmd);
+
+		pipeline->_offsets.insert(std::make_pair(target, offset));
+	}
+	void patch_pipeline::remove_state::finalize(update_state& ustate)
+	{
+		auto it = pipeline->_offsets.find(target);
+		if (it == pipeline->_offsets.end())
+			assert(false);
+
+		GLuint offset = it->second;
+
+		MoveCommand mvcmd;
+		mvcmd.is_new = GL_FALSE;
+		mvcmd.source = 0;
+		mvcmd.dest = offset;
+
+		ustate.movecommands.push_back(mvcmd);
+
+		pipeline->_offsets.erase(it);
+		pipeline->manager().dealloc_block(offset);
+	}
+
+	patch_pipeline::generate_state::generate_state(std::shared_ptr<patch> tgt, patch_pipeline* pipeline) :
+		target(tgt),
+		counter(0),
+		pipeline(pipeline),
+		offset(0)
+	{
+		buffers[0] = 0;
+		buffers[1] = 0;
+	}
+	patch_pipeline::remove_state::remove_state(std::shared_ptr<patch> tgt, patch_pipeline* pipeline) :
+		target(tgt),
+		counter(0),
+		pipeline(pipeline)
+	{
+
 	}
 }
--- render2/patch_pipeline.h
+++ render2/patch_pipeline.h
@@ -62,133 +62,49 @@ namespace planet_engine
 		static constexpr size_t VERTEX_BUFFER_SIZE = (SIDE_LEN + 2) * (SIDE_LEN + 2) * sizeof(float) * 4;
 		static constexpr size_t NUM_INVOCATIONS = ((SIDE_LEN + 2) * (SIDE_LEN + 2) + SHADER_GROUP_SIZE - 1) / SHADER_GROUP_SIZE;
 		static constexpr size_t GEN_VERTEX_INVOCATIONS = (SIDE_LEN + 2 + 7) / 7;
+		static constexpr size_t CANCELLED_COUNTER_VALUE = 0xFF;
 
-		struct _upsample_state
+		struct generate_state
 		{
-			enum execution_state : uint_least8_t
-			{
-				START = 0,
-				DONE = 1,
-				ERROR_STATE = 0xFF
-			};
-
-			execution_state state;
-			patch_pipeline* pipeline;
-			std::shared_ptr<patch> target_patch;
-			// Order: NW, NE, SW, SE
-			std::shared_ptr<patch> children[4];
-
-			GLuint offset;
-
-			bool is_done() const;
-			void step();
-
-			_upsample_state(std::shared_ptr<patch> p, patch_pipeline* pipeline);
-		};
-		struct _remove_state
-		{
-			enum execution_state : uint_least8_t
-			{
-				START = 0,
-				DONE = 1,
-				ERROR_STATE = 0xFF
-			};
-
-			execution_state state;
-			patch_pipeline* pipeline;
-			std::shared_ptr<patch> target_patch;
-
-			GLuint offset;
-
-			size_t ctr;
-
-			bool is_done() const;
-			void step();
-
-			_remove_state(std::shared_ptr<patch> p, patch_pipeline* pipeline);
-		};
-
-		struct subdivide_state
-		{
+			std::shared_ptr<patch> target;
 			size_t counter;
 			patch_pipeline* pipeline;
-
-			GLuint buffers[4][2]
-
-			GLuint offsets[4];
-			std::shared_ptr<patch> children[4];
-			GLuint offset;
-			std::shared_ptr<patch> parent;
-
-			void gen_vertices(size_t idx);
-			void gen_mesh(size_t idx);
-
-			subdivide_state(std::shared_ptr<patch> p, patch_pipeline* pipeline);
-
-			bool can_finalize();
-			void get_state(update_state& ustate);
-		};
-		struct merge_state
-		{
-			size_t counter;
-			patch_pipeline* pipeline;
-
-			std::shared_ptr<patch> parent;
-			std::shared_ptr<patch> children[4];
-
-			GLuint offset;
-			GLuint offsets[4];
 
 			GLuint buffers[2];
+			GLuint offset;
 
-			merge_state(std::shared_ptr<patch> patch, patch_pipeline* pipeline);
+			void cancel();
 
-			void gen_vertices();
-			void gen_mesh();
+			bool can_finalize() const;
+			void finalize(update_state& ustate);
 
-			bool can_finalize();
-			void get_state(update_state& ustate);
+			generate_state(std::shared_ptr<patch> tgt, patch_pipeline* pipeline);
 		};
 		struct remove_state
 		{
-			remove_state(std::shared_ptr<patch> patch, patch_pipeline* pipeline);
-
-			bool can_finalize();
-			void get_state(update_state& ustate);
-
-			std::shared_ptr<patch> patch;
-			patch_pipeline* pipeline;
-		};
-		struct generate_state
-		{
-			generate_state(std::shared_ptr<patch> patch, patch_pipeline* pipeline);
-
-			void gen_vertices();
-			void gen_mesh();
-
-			bool can_finalize();
-			void get_state(update_state& ustate);
-
+			std::shared_ptr<patch> target;
 			size_t counter;
 			patch_pipeline* pipeline;
 
-			std::shared_ptr<patch> patch;
-			GLuint offset;
+			void cancel();
 
-			GLuint uniforms;
-			GLuint vertex_buffer;
+			bool can_finalize() const;
+			void finalize(update_state& ustate);
+
+			remove_state(std::shared_ptr<patch> tgt, patch_pipeline* pipeline);
 		};
 
 		typedef util::any_of<
-			std::shared_ptr<subdivide_state>,
-			std::shared_ptr<merge_state>,
 			std::shared_ptr<remove_state>,
 			std::shared_ptr<generate_state>> exec_type;
 
 		buffer_manager _manager;
 
-		std::queue<std::function<void()>> _job_queue;
-		std::queue<exec_type> _exec_queue;
+		// Returns a boolean indicating if the job
+		// had been cancelled. False indicates that
+		// the job had been cancelled
+		std::queue<std::function<bool()>> _job_queue;
+		std::deque<exec_type> _exec_queue;
 
 		std::map<std::shared_ptr<patch>, GLuint> _offsets;
 
@@ -197,7 +113,7 @@ namespace planet_engine
 		GLuint _upsampler;
 
 		void gen_vertices(GLuint buffers[2], std::shared_ptr<patch> patch, GLuint* offset);
-		void gen_mesh(GLuint buffers[2], std::shared_ptr<patch> patch, GLuint* offset);
+		void gen_mesh(GLuint buffers[2], std::shared_ptr<patch> patch, const GLuint* offset);
 
 	public:
 		patch_pipeline(size_t num_blocks);
@@ -205,18 +121,12 @@ namespace planet_engine
 		patch_pipeline(patch_pipeline&&) = delete;
 		~patch_pipeline();
 
-		void subdivide(std::shared_ptr<patch> patch);
-		void merge(std::shared_ptr<patch> patch);
 		void generate(std::shared_ptr<patch> patch);
 		void remove(std::shared_ptr<patch> patch);
-
-		void cull();
 
 		buffer_manager& manager();
 		const decltype(_offsets)& patches() const;
 
 		update_state process(size_t n = 4);
-
-		static bool can_upsample(std::shared_ptr<patch> patch);
 	};
 }
